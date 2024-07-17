5befb797aa5b77be4b8f4ec3baf335cfe77b7e1c
muh-younus <braq369@gmail.com>
2024-07-17 16:30:11 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let div = document.querySelectorAll(".div");
+let index =0;
+let val;
+for(let divs of div){
+
+   val= divs[index];
+
+   if (val == divs[index]){
+
+    divs[index].innerText = "Muhammad Younus";
+    
+   }
+   else if (val == divs[index]){
+
+    divs[index].innerText = "Alina";
+    
+   }
+
+    
+}
